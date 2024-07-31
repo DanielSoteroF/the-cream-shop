d1cdc7546261100c5366af8e2bdc5db22bdcd386
Alonso Auqui <andriod.pvp@gmail.com>
2024-07-30 22:42:18 -0500

    ✅ Navbar y footer agregado, falta implementar el modo responsivo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary-red': '#F20505',
+				'neutral-white': '#EDF2E9 ',
+				'neutral-black': '#0D0D0D',
+				'secundary-purple': '#A732D9',
+				'secundary-green': '#14D959',
+			}
+		},
 	},
 	plugins: [],
 }
