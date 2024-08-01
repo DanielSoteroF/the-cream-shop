7aedc39ecd6b55f582b5632550ee3c3f5942665c
DanielSotero <danielsoterofarfan1@gmail.com>
2024-08-01 17:56:45 -0500

    ✔️ Collections Agregado

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,7 @@ export default {
 				'primary-red': '#F20505',
 				'neutral-white': '#EDF2E9 ',
 				'neutral-black': '#0D0D0D',
+				'neutral-gray': '#9C9C9C',
 				'secundary-purple': '#A732D9',
 				'secundary-green': '#14D959',
 			},
