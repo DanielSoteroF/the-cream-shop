f9412c071a0722d2f8cd7bbd69a12bb8a09e4e2d
DanielSotero <danielsoterofarfan1@gmail.com>
2024-08-01 20:40:19 -0500

    ✔️ Sneakers Agregados 24

--- src/assets/data/products.js
+++ src/assets/data/products.js
@@ -1,6 +1,10 @@
-import AirJordanRetro4 from '../img-collections/air_jordan_retro_4.webp';
-import Aj4 from '../img-collections/aj4.webp';
-import NikeAirForce from '../img-collections/nike-air-force.webp';
+
+import AirJordanRetro4 from '../img-collections/air-jordan-retro-4.webp'
+import Aj4 from '../img-collections/aj4.webp'
+import NikeAirForce from '../img-collections/nike-air-force.webp'
+import NikeAirForce1MidOffWhite from '../img-collections/nike-air-force-1-mid-off-white.webp'
+import NikeSbApril from '../img-collections/nike-sb-april.webp'
+import NikeAirForceBlack from '../img-collections/nike-air-force-black.webp'
 
 const COLLECTIONS = [
     {
@@ -17,6 +21,22 @@ const COLLECTIONS = [
         name: 'Nike Air Force',
         img: NikeAirForce.src,
         description: 'Kit de limpieza con una variedad de productos para mantener tu interior limpio y fresco.',
+    },
+    {
+        name: 'Nike Air Force 1 Mid Off White',
+        img: NikeAirForce1MidOffWhite.src,
+        description: 'Kit de limpieza con una variedad de productos para mantener tu interior limpio y fresco.',
+    },
+    {
+        name: 'Nike SB April',
+        img: NikeSbApril.src,
+        description: 'Kit de limpieza con una variedad de productos para mantener tu interior limpio y fresco.',
+    },
+    {
+        name: 'Nike Air Force Black',
+        img: NikeAirForceBlack.src,
+        description: 'Kit de limpieza con una variedad de productos para mantener tu interior limpio y fresco.',
+
     }
 ];
 
